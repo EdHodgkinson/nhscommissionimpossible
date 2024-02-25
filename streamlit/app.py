0b50e1afa1a35df6f7d2b678926297bc0557acd6
EdHodgkinson <91339018+EdHodgkinson@users.noreply.github.com>
2024-02-25 11:39:55 +0000

    added aggregate selection per measure

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,13 +12,22 @@ st.code(f'df.drop(columns=({drop_cols_str}))')
 
 
 edited_df = st.data_editor(df, num_rows="dynamic")
+
 map_df = duckdb.sql('SELECT * FROM edited_df WHERE lat IS NOT NULL').df()
 st.map(data=map_df,latitude ='lat',longitude='long')
 
 
 group_col = st.selectbox(label='Select a grouping column',options=df.columns)
-measure_col = st.selectbox(label='Select a measure column',options=df.columns)
-aggregate = st.selectbox(label='Select aggregate for measure',options=('Sum','Avg'))
+measure_cols = st.multiselect(label='Select a measure column',options=df.columns)
 
-agg_df = duckdb.sql(f'SELECT {group_col}, {aggregate}({measure_col}) FROM edited_df GROUP BY {group_col} ').df()
+measure_cols_agg = []
+for measure in measure_cols:
+    measure_str = st.selectbox(label=f'Select aggregate for {measure}',options=('Sum','Avg'), key=measure) + f'({measure})'
+    st.write(measure_str)
+    measure_cols_agg.append(measure_str)
+
+measure_cols_str = ",".join(measure_cols_agg)
+st.write(measure_cols_str)
+
+agg_df = duckdb.sql(f'SELECT {group_col}, {measure_cols_str} FROM edited_df GROUP BY {group_col} ').df()
 st.dataframe(agg_df)
